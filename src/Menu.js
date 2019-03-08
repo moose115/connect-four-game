aa38b4c113a45fa4fdad669a4376959fa239b6c5
Mustafa Ucuncu <mustafaucuncu@onet.pl>
2019-03-08 12:13:31 +0100

    Add hover effect on menu buttons

--- src/Menu.js
+++ src/Menu.js
@@ -24,6 +24,18 @@ export default class Menu {
         this.button[i].function(this.app, this);
         this.toggleMenu();
       });
+      rectangle.on('mouseover', () => {
+        rectangle.lineStyle(2, 0x555555);
+        rectangle.beginFill(0x666666);
+        rectangle.drawRect(64, 2*i*64 + 64, 5*64, 64);
+        rectangle.endFill();
+      });
+      rectangle.on('mouseout', () => {
+        rectangle.lineStyle(2, 0x777777);
+        rectangle.beginFill(0x888888);
+        rectangle.drawRect(64, 2*i*64 + 64, 5*64, 64);
+        rectangle.endFill();
+      });
       this.menuScene.addChild(rectangle);
 
       const text = new PIXI.Text();
