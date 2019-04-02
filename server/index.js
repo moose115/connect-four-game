26f5f44bf591108aa1483ec5b81e5f52038dc3d9
Mustafa Ucuncu <mustafaucuncu@onet.pl>
2019-04-02 03:00:59 +0200

    Port env field to uppercase

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const colyseus = require('colyseus');
 const http = require('http');
 const express = require('express');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 const expApp = express();
